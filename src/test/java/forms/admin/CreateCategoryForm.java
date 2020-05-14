dbd8da505f3715fe2647fc7b4b2599796b99d550
Oleg N <olegn@mail.com>
2020-05-15 00:33:58 +0300

    update category test and form

--- src/test/java/forms/admin/CreateCategoryForm.java
+++ src/test/java/forms/admin/CreateCategoryForm.java
@@ -3,6 +3,7 @@ package forms.admin;
 import models.Category;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 
 public class CreateCategoryForm {
@@ -10,6 +11,15 @@ public class CreateCategoryForm {
     public void fillCreateForm(Category category) {
         $("#input-name1").setValue(category.getName());
         $("#input-meta-title1").setValue(category.getMetaTagTitle());
+        $("div.note-editable").setValue(category.getDescription());
+        if (category.isTop()) {
+            gotoTab("Data");
+            $("#input-top").click();
+        }
+    }
+
+    public void gotoTab(String title) {
+        $(byText(title)).click();
     }
 
     public void saveCreateForm() {
--- src/test/java/tests/CategoryTest.java
+++ src/test/java/tests/CategoryTest.java
@@ -1,10 +1,12 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
 import forms.admin.*;
 import models.*;
 import org.testng.annotations.Test;
 import webdriver.BaseTest;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CategoryTest extends BaseTest {
@@ -20,6 +22,13 @@ public class CategoryTest extends BaseTest {
     private final boolean topCategory = true;
 
     Category category = new Category(titleCategory, metaTagTitleCategory, descriptionCategory, topCategory);
+
+    private final String titleCategoryAfterUpdate = "Test category after update";
+    private final String metaTagTitleCategoryAfterUpdate = "TestMetaTagTitle after update";
+    private final String descriptionCategoryAfterUpdate = "Test Description after update";
+
+    Category categoryAfterUpdate = new Category(titleCategoryAfterUpdate, metaTagTitleCategoryAfterUpdate,
+            descriptionCategoryAfterUpdate, topCategory);
 
     @Test(description = "Проверка создания группы")
     public void createGroupTest() {
@@ -48,14 +57,45 @@ public class CategoryTest extends BaseTest {
     @Test(description = "Проверка отображения категории", dependsOnMethods = "createGroupTest")
     public void readCategoryTest() {
 
+        open("/");
+
+        $(byText(category.getName())).shouldBe(Condition.visible);
+
     }
 
     @Test(description = "Обновление категории", dependsOnMethods = "readCategoryTest")
     public void updateCategoryTest() {
 
+        AdminLoginForm adminLoginForm = new AdminLoginForm();
+        NavigationMenuForm navigationMenuForm = new NavigationMenuForm();
+        CategoriesForm categoriesForm = new CategoriesForm();
+        CreateCategoryForm createCategoryForm = new CreateCategoryForm();
+
+        // Login
+        open("/admin");
+        adminLoginForm.login(admin);
+
+        // Navigate to categories page
+        navigationMenuForm.gotoPage("Catalog", "Categories");
+
+        // Open create category form
+        categoriesForm.gotoCreateForm();
+
+        // Fill form
+        createCategoryForm.fillCreateForm(categoryAfterUpdate);
+        createCategoryForm.saveCreateForm();
+
     }
 
-    @Test(description = "Удаление категории", dependsOnMethods = "updateCategoryTest", alwaysRun = true)
+    @Test(description = "Проверка отображения категории после обновления", dependsOnMethods = "updateCategoryTest")
+    public void readCategoryAfterUpdateTest() {
+
+        open("/");
+
+        $(byText(categoryAfterUpdate.getName())).shouldBe(Condition.visible);
+    }
+
+    @Test(description = "Удаление категории", dependsOnMethods = "readCategoryAfterUpdateTest", alwaysRun = true)
     public void deleteCategory() {
         AdminLoginForm adminLoginForm = new AdminLoginForm();
         NavigationMenuForm navigationMenuForm = new NavigationMenuForm();
@@ -68,4 +108,14 @@ public class CategoryTest extends BaseTest {
         navigationMenuForm.gotoPage("Catalog", "Categories");
 
     }
+
+    @Test(description = "Проверка отображения категории после удаления", dependsOnMethods = "deleteCategory")
+    public void readCategoryAfterDeleteTest() {
+
+        open("/");
+
+        $(byText(categoryAfterUpdate.getName())).shouldNotBe(Condition.visible);
+
+    }
+
 }

fb101bac3ead9cbff19b2ba864936dcae0a85d55
Oleg N <olegn@mail.com>
2020-05-15 17:17:11 +0300

    update basetest

--- src/test/java/webdriver/BaseTest.java
+++ src/test/java/webdriver/BaseTest.java
@@ -10,23 +10,15 @@ public class BaseTest {
 
     protected String adminLogin, adminPassword;
 
-    private String rootLink, browser;
-
     @BeforeTest
     @Parameters({"adminLogin", "adminPassword", "rootLink", "browser"})
     public void readParams(String adminLogin, String adminPassword, String rootLink, String browser) {
         this.adminLogin = adminLogin;
         this.adminPassword = adminPassword;
-        this.rootLink = rootLink;
-        this.browser = browser;
         Configuration.baseUrl = rootLink;
         Configuration.browser = browser;
         Configuration.startMaximized = true;
         Configuration.timeout = 5000;
-    }
-
-    @BeforeTest
-    public void configuration() {
     }
 
     public void logStep(Integer num, String description) {

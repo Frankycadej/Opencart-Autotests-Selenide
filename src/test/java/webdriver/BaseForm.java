6390434b2ece482c91bfe5f07e61f0563a76017e
Oleg N <olegn@mail.com>
2020-05-15 10:40:04 +0300

    update BaseForm

--- src/test/java/webdriver/BaseForm.java
+++ src/test/java/webdriver/BaseForm.java
@@ -1,4 +1,11 @@
 package webdriver;
 
+import com.codeborne.selenide.Selenide;
+
 public class BaseForm {
+
+    public void submitAlert() {
+        Selenide selenide = new Selenide();
+        selenide.confirm();
+    }
 }

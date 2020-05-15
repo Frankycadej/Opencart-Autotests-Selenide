c4e3cfe204474e2fbc9d88e516d6a180f9fb5a95
Oleg N <olegn@mail.com>
2020-05-15 14:33:53 +0300

    add parameters in basetest

--- src/test/java/webdriver/BaseTest.java
+++ src/test/java/webdriver/BaseTest.java
@@ -2,21 +2,35 @@ package webdriver;
 
 import com.codeborne.selenide.Configuration;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 import static io.qameta.allure.Allure.step;
 
 public class BaseTest {
 
+    protected String adminLogin, adminPassword;
+
+    private String rootLink, browser;
+
+    @BeforeTest
+    @Parameters({"adminLogin", "adminPassword", "rootLink", "browser"})
+    public void readParams(String adminLogin, String adminPassword, String rootLink, String browser) {
+        this.adminLogin = adminLogin;
+        this.adminPassword = adminPassword;
+        this.rootLink = rootLink;
+        this.browser = browser;
+    }
+
     @BeforeTest
     public void configuration() {
-        Configuration.baseUrl = "http://opencart.nakoskin.ru";
-        Configuration.browser = "chrome";
+        Configuration.baseUrl = rootLink;
+        Configuration.browser = browser;
         Configuration.startMaximized = true;
         Configuration.timeout = 5000;
     }
 
     public void logStep(Integer num, String description) {
-        step(num + description);
+        step(num + " " + description);
     }
 
 }

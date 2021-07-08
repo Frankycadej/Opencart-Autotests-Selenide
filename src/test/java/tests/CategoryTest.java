689dd6e6956096f93d2328a1d9fde966bce0e209
0xd0a2 <0xd0a2@xviiicen.sc>
2021-07-08 17:11:57 +0300

    update CategoryTest

--- src/test/java/tests/CategoryTest.java
+++ src/test/java/tests/CategoryTest.java
@@ -1,20 +1,24 @@
 package tests;
 
-import com.codeborne.selenide.Condition;
-import forms.admin.*;
 import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
 import models.Category;
 import models.User;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import steps.HomePageSteps;
+import steps.admin.*;
 import webdriver.BaseTest;
 
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-
+@Feature("Create - Read - Update - Delete Test")
 public class CategoryTest extends BaseTest {
+    private final AdminLoginFormSteps adminLoginFormSteps = new AdminLoginFormSteps();
+    private final NavigationMenuFormSteps navigationMenuFormSteps = new NavigationMenuFormSteps();
+    private final CategoriesFormSteps categoriesFormSteps = new CategoriesFormSteps();
+    private final CreateCategoryFormSteps createCategoryFormSteps = new CreateCategoryFormSteps();
+    private final UpdateCategoryFormSteps updateCategoryFormSteps = new UpdateCategoryFormSteps();
+    private final HomePageSteps homePageSteps = new HomePageSteps();
 
     User admin;
     Category category;
@@ -36,101 +40,64 @@ public class CategoryTest extends BaseTest {
     @Description(value = "Создание категории")
     public void createGroupTest() {
 
-        open("https://opencart.nakoskin.ru/admin");
+        openAdminLoginPage();
 
-        logStep(1, "Login as admin");
-        AdminLoginForm adminLoginForm = new AdminLoginForm();
-        adminLoginForm.login(admin);
-
-        logStep(2, "Navigate to categories page");
-        NavigationMenuForm navigationMenuForm = new NavigationMenuForm();
-        navigationMenuForm.gotoPage("Catalog", "Categories");
-
-        logStep(3, "Open create category form");
-        CategoriesForm categoriesForm = new CategoriesForm();
-        categoriesForm.gotoCreateForm();
-
-        logStep(3, "Fill form");
-        CreateCategoryForm createCategoryForm = new CreateCategoryForm();
-        createCategoryForm.fillCreateForm(category);
-
-        logStep(4, "Save form");
-        createCategoryForm.saveCreateForm();
-
+        adminLoginFormSteps.login(admin);
+        navigationMenuFormSteps.gotoPage("Catalog", "Categories");
+        categoriesFormSteps.gotoCreateForm();
+        createCategoryFormSteps.fillCreateForm(category)
+                .saveCreateForm();
     }
 
     @Test(description = "Проверка отображения категории", dependsOnMethods = "createGroupTest")
     @Description(value = "Проверка отображения категории")
     public void readCategoryTest() {
-        open("https://opencart.nakoskin.ru/");
 
-        logStep(1, "Check that category is visible");
-        $(byText(category.getName())).shouldBe(Condition.visible);
+        openMainPage();
+
+        homePageSteps.checkThatCategoryIsVisible(category.getName());
     }
 
     @Test(description = "Обновление категории", dependsOnMethods = "readCategoryTest")
     @Description(value = "Обновление категории")
     public void updateCategoryTest() {
 
-        open("https://opencart.nakoskin.ru/admin");
+        openAdminLoginPage();
 
-        logStep(1, "Login as admin");
-        AdminLoginForm adminLoginForm = new AdminLoginForm();
-        adminLoginForm.login(admin);
-
-        logStep(2, "Navigate to categories page");
-        NavigationMenuForm navigationMenuForm = new NavigationMenuForm();
-        navigationMenuForm.gotoPage("Catalog", "Categories");
-
-        logStep(3, "Find and mark category's checkbox");
-        CategoriesForm categoriesForm = new CategoriesForm();
-        categoriesForm.findCategoryAndClickOnUpdateBtn(category);
-
-        logStep(3, "Fill form");
-        UpdateCategoryForm updateCategoryForm = new UpdateCategoryForm();
-        updateCategoryForm.fillCreateForm(categoryAfterUpdate);
-
-        logStep(4, "Save form");
-        updateCategoryForm.saveCreateForm();
+        adminLoginFormSteps.login(admin);
+        navigationMenuFormSteps.gotoPage("Catalog", "Categories");
+        categoriesFormSteps.findCategoryAndClickOnUpdateBtn(category);
+        updateCategoryFormSteps.fillCreateForm(categoryAfterUpdate)
+                .saveCreateForm();
     }
 
     @Test(description = "Проверка отображения категории после обновления", dependsOnMethods = "updateCategoryTest")
     @Description(value = "Проверка отображения категории после обновления")
     public void readCategoryAfterUpdateTest() {
-        open("https://opencart.nakoskin.ru/");
 
-        logStep(1, "Check that category is visible");
-        $(byText(categoryAfterUpdate.getName())).shouldBe(Condition.visible);
+        openMainPage();
+
+        homePageSteps.checkThatCategoryIsVisible(categoryAfterUpdate.getName());
     }
 
     @Test(description = "Удаление категории", dependsOnMethods = "readCategoryAfterUpdateTest", alwaysRun = true)
     @Description(value = "Удаление категории")
     public void deleteCategory() {
 
-        open("https://opencart.nakoskin.ru/admin");
+        openAdminLoginPage();
 
-        logStep(1, "Login as admin");
-        AdminLoginForm adminLoginForm = new AdminLoginForm();
-        adminLoginForm.login(admin);
-
-        logStep(2, "Navigate to categories page");
-        NavigationMenuForm navigationMenuForm = new NavigationMenuForm();
-        navigationMenuForm.gotoPage("Catalog", "Categories");
-
-        logStep(3, "Find category in table and mark it");
-        CategoriesForm categoriesForm = new CategoriesForm();
-        categoriesForm.findAndMarkCategory(categoryAfterUpdate);
-
-        logStep(5, "Click on delete button");
-        categoriesForm.clickOnDeleteBtn();
-
+        adminLoginFormSteps.login(admin);
+        navigationMenuFormSteps.gotoPage("Catalog", "Categories");
+        categoriesFormSteps.findAndMarkCategory(categoryAfterUpdate)
+                .clickOnDeleteBtn();
     }
 
     @Test(description = "Проверка отображения категории после удаления", dependsOnMethods = "deleteCategory")
     @Description(value = "Проверка отображения категории после удаления")
     public void readCategoryAfterDeleteTest() {
-        open("https://opencart.nakoskin.ru/");
 
-        $(byText(categoryAfterUpdate.getName())).exists();
+        openMainPage();
+
+        homePageSteps.checkThatCategoryIsNotVisible(categoryAfterUpdate.getName());
     }
 }
